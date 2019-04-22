5072db3154dd3e29fac211d4cd0bb49dde1f186a
Cesar Piangers <cesarpiangers@gmail.com>
2019-04-22 16:33:50 -0300

    Vs Qgis 3.4 Dev

--- SpatialFilter.py
+++ SpatialFilter.py
@@ -8,104 +8,23 @@ from qgis.core import *
 from SpatialFilter import resources_rc  
 import os
 
+
 class SpatialFilter():
     
 
     def __init__(self, iface):
-            
-        # Save reference to the QGIS interface   
         self.iface = iface
-        # initialize plugin directory
-        self.plugin_dir = os.path.dirname(__file__)
-        # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'BGTImport_{}.qm'.format(locale))
-
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-
-            if qVersion() > '4.3.3':
-                QCoreApplication.installTranslator(self.translator)
-
-
-        self.actions = []
-        self.menu = self.tr(u'&Batch Vector Layer Saver')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'BatchVectorLayerSaver')
-        self.toolbar.setObjectName(u'SpatialFiler')
-
-       
-    def tr(self, message):
+		
+    def initGui(self): 
+		
+        # cria uma ação que iniciará a configuração do plugin 
         
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('BatchVectorLayerSaver', message)
+        self.initVariables()
+        self.initSignals()
         
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-       
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToVectorMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
-
-    def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
-        self.coordinates = []
-        icon_path = ':/plugins/SpatialFilter/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'SpatialFiler'),
-            callback=self.initSignals,
-            parent=self.iface.mainWindow())
-            
-        self.previousMapTool = self.iface.mapCanvas().mapTool()
-        self.myMapTool = QgsMapToolEmitPoint( self.iface.mapCanvas() )
-        self.isEditing = 0
-
-    def unload(self):
-        """Removes the plugin menu item and icon from QGIS GUI."""
-        for action in self.actions:
-            self.iface.removePluginVectorMenu(
-                self.tr(u'&SpatialFiler'),
-                action)
-            self.iface.removeToolBarIcon(self.action)
-        # remove the toolbar
-        del self.toolbar
-
-    
+        
     def initVariables(self):
-        
+        self.coordinates = []
 
         # Criação da action e da toolbar
 
@@ -119,7 +38,9 @@ class SpatialFilter():
         self.action.setCheckable(True)
         self.toolbar.addAction(self.action)
 
-        
+        self.previousMapTool = self.iface.mapCanvas().mapTool()
+        self.myMapTool = QgsMapToolEmitPoint( self.iface.mapCanvas() )
+        self.isEditing = 0
        
 
     def initSignals(self):
@@ -128,12 +49,12 @@ class SpatialFilter():
 
     def RubberBand(self, bolean):
         if bolean:
-            self.myRubberBand = QgsRubberBand( self.iface.mapCanvas(), QGis.Polygon )
+            self.myRubberBand = QgsRubberBand( self.iface.mapCanvas(), QgsWkbTypes.PolygonGeometry )
             color = QColor(78, 97, 114)
             color.setAlpha(190)
             self.myRubberBand.setColor(color)
             self.myRubberBand.setFillColor(QColor(255, 0, 0, 40))
-            self.myRubberBand.setBorderColor(QColor(255, 0, 0, 200))
+            #self.myRubberBand.setBorderColor(QColor(255, 0, 0, 200))
             
             # Set MapTool
             self.iface.mapCanvas().setMapTool( self.myMapTool )
@@ -159,12 +80,12 @@ class SpatialFilter():
         self.action.setCheckable(True)
 
     def unload(self):
-        self.disconnect()        
+        self.disconnect()
 
     def mouseClick( self, currentPos, clickedButton ):
         if clickedButton == Qt.LeftButton:# and myRubberBand.numberOfVertices() == 0: 
-            self.myRubberBand.addPoint( QgsPointXY(currentPos) )
-            self.coordinates.append( QgsPointXY(currentPos) )
+            self.myRubberBand.addPoint( QgsPointXY(currentPos))
+            self.coordinates.append( QgsPointXY(currentPos))
             self.isEditing = 1
             
         elif clickedButton == Qt.RightButton and self.myRubberBand.numberOfVertices() > 2:
@@ -175,21 +96,21 @@ class SpatialFilter():
             poly = QgsFeature() 
             geomP = self.myRubberBand.asGeometry()
             poly.setGeometry(geomP)
-            g=geomP.exportToWkt() # Get WKT coordenates.
+            g = geomP.asWkt() # Get WKT coordenates.
 
             canvas=self.iface.mapCanvas()
 
-            c = canvas.mapRenderer().destinationCrs().authid() # Get EPSG.
-            rep = c.replace("EPSG:","") 
-
+            c = canvas.mapSettings().destinationCrs().authid() # Get EPSG.
+            rep = c.replace("EPSG:","")
+  
             vlyr = QgsVectorLayer("?query=SELECT geom_from_wkt('%s') as geometry&geometry=geometry:3:%s"%(g,rep), "Polygon_Reference", "virtual")
             
-            QgsMapLayerRegistry.instance().addMapLayer(vlyr)
+            QgsProject.instance().addMapLayer(vlyr)
 
-            self.myRubberBand.reset(QGis.Polygon)
- 
+            self.myRubberBand.reset(QgsWkbTypes.PolygonGeometry)
+
             string = U"st_intersects(geom,st_geomfromewkt('SRID="+rep+";"+g+"'))"
-             
+            
             layers = self.iface.mapCanvas().layers()
             
             for layer in layers:
@@ -198,15 +119,10 @@ class SpatialFilter():
                 except Exception:
                     pass
             
-            self.myRubberBand.reset(QGis.Polygon)
+            self.myRubberBand.reset(QgsWkbTypes.PolygonGeometry)
             self.disconnect()
             self.unChecked()
 
     def mouseMove( self, currentPos ):
         if self.isEditing == 1:
             self.myRubberBand.movePoint(QgsPointXY(currentPos))
-
-
-
-		
-	
